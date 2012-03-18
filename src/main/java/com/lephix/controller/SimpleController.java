5344a36a7d82a04573931c4fb6bf8154116a8569
Lephix <lephix@163.com>
2012-03-18 18:45:37 +0800

    new file:   "spring3mvc\325\346\325\375\310\353\303\305\327\312\301\317.doc" 	modified:   src/main/java/com/lephix/controller/SimpleController.java

--- src/main/java/com/lephix/controller/SimpleController.java
+++ src/main/java/com/lephix/controller/SimpleController.java
@@ -4,13 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SimpleController {
 
 	Logger log = LoggerFactory.getLogger(SimpleController.class);
 	
-	@RequestMapping("/simple")
+	@RequestMapping(value = "/simple", method = RequestMethod.GET)
 	public String simplePage() throws Exception {
 		log.debug("haha");
 		return "simple";
