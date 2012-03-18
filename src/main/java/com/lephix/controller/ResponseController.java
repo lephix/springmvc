722ebf5e0b68cf61ba79132a93a6167d2fd62b4c
Lephix <lephix@163.com>
2012-03-18 23:11:57 +0800

    modified:   src/main/java/com/lephix/controller/ResponseController.java 	modified:   src/main/java/com/lephix/controller/SimpleController.java

--- src/main/java/com/lephix/controller/ResponseController.java
+++ src/main/java/com/lephix/controller/ResponseController.java
@@ -1,14 +1,39 @@
 package com.lephix.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/response/*")
 public class ResponseController {
 
+	@RequestMapping (value = "1", produces = "text/plain;charset=ISO-8859-1")
 	public @ResponseBody String response1() throws Exception {
-		return "";
+		return "你好北京 with encoding ISO-8859-1";
+	}
+
+	@RequestMapping (value = "2", produces = "text/plain;charset=UTF-8")
+	public @ResponseBody String response2() throws Exception {
+		return "你好北京 with encoding UTF-8";
+	}
+	
+	@RequestMapping (value = "2", params = {"foo"}) 
+	public @ResponseBody String response3(@RequestParam String foo) throws Exception {
+		return "你好北京 without explicit encoding code but with request parameter foo = " + foo;
+	}
+	
+	@RequestMapping(value="3", method=RequestMethod.GET)
+	public ResponseEntity<String> responseEntityCustomHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_PLAIN);
+		return new ResponseEntity<String>("The String ResponseBody with custom header Content-Type=text/plain",
+				headers, HttpStatus.OK);
 	}
 }
--- src/main/java/com/lephix/controller/SimpleController.java
+++ src/main/java/com/lephix/controller/SimpleController.java
@@ -1,19 +1,65 @@
 package com.lephix.controller;
 
+import java.security.Principal;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/simple/*")
 public class SimpleController {
 
 	Logger log = LoggerFactory.getLogger(SimpleController.class);
-	
-	@RequestMapping(value = "/simple", method = RequestMethod.GET)
+
+	@RequestMapping(value = "1", method = RequestMethod.GET)
 	public String simplePage() throws Exception {
-		log.debug("haha");
+		log.debug("First simple sample for controller!");
 		return "simple";
+	}
+
+	@RequestMapping(value = "2")
+	public @ResponseBody
+	String simple2Page() throws Exception {
+		log.debug("Simple2 using @ResponseBody annotation");
+		return "Simple2 using @ResponseBody annotation";
+	}
+
+	@RequestMapping(value = "3")
+	public @ResponseBody
+	String simple3Page(@RequestParam String foo) throws Exception {
+		return "Simple3 with params foo = " + foo;
+	}
+
+	@RequestMapping(value = "4/{foo}")
+	public @ResponseBody
+	String simple4Page(@PathVariable String foo) throws Exception {
+		return "Simple4 with path foo = " + foo;
+	}
+
+	@RequestMapping(value = "5")
+	public @ResponseBody
+	String simple5Page(@RequestBody String body) throws Exception {
+		return "Simple5 with @RequestBody = " + body;
+	}
+
+	@RequestMapping(value = "requestArgs", method = RequestMethod.GET)
+	public @ResponseBody
+	String standardRequestArgs(Principal user, HttpServletRequest request, 
+			Locale locale) {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append("request = ").append(request).append(", ");
+		buffer.append("userPrincipal = ").append(user).append(", ");
+		buffer.append("requestLocale = ").append(locale);
+		return buffer.toString();
 	}
 }
